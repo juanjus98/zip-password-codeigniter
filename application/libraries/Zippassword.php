3e89c791f5434949955afc134df80618a4c79413
Juan Julio Sandoval Layza <juanjus98@gmail.com>
2017-09-21 16:53:23 -0500

    Inicializando proyecto

---
+++ application/libraries/Zippassword.php
@@ -0,0 +1,21 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Zippassword Class
+* Proteger con password un archivo zip en codeigniter
+*
+* @author Juan Julio Sandoval <juanjus98@gmail.com>
+* @author Luis Rodriguez <luizmirodriguez@gmail.com >
+*/
+
+class Zippassword {
+
+ function __construct() {
+
+ }
+
+ function addPassword(){
+
+ }
+
+}
